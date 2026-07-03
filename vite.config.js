4270f9b4c4ad88d3dea510c15f681affc32445d7
Saksham Singh Chauhan <142093035+rustymofo@users.noreply.github.com>
2026-07-03 17:11:20 +0530

    Fix: explicit vite build config

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
