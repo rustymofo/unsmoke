bc05a0d71fa156de2466ccf476d654dd4b2066f1
Saksham Singh Chauhan <142093035+rustymofo@users.noreply.github.com>
2026-07-03 22:14:54 +0530

    SW killer v4

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,10 @@
-// This service worker unregisters itself and clears all caches
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.registration.unregister())
-  );
-});
+// Unsmoke SW Killer v4
+const kill = async () => {
+  const regs = await navigator.serviceWorker.getRegistrations();
+  await Promise.all(regs.map(r => r.unregister()));
+  const keys = await caches.keys();
+  await Promise.all(keys.map(k => caches.delete(k)));
+};
+self.addEventListener('install', e => { e.waitUntil(kill()); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(kill().then(() => self.registration.unregister())); });
+self.addEventListener('fetch', () => {});
