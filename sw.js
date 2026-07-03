4c3dd2c770988723e5bfd315d1a6b03acbc675a0
Saksham Singh Chauhan <142093035+rustymofo@users.noreply.github.com>
2026-07-03 17:29:43 +0530

    Deploy sw.js

---
+++ sw.js
@@ -0,0 +1,9 @@
+// This service worker unregisters itself and clears all caches
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+  );
+});
